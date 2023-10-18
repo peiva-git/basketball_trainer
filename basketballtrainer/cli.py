a9768bd65c6df044475038852d4ad24397c76711
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-10-18 23:17:15 +0200

    Added cli for training

--- basketballtrainer/cli.py
+++ basketballtrainer/cli.py
@@ -1,1 +1,41 @@
+import argparse
 
+from paddleseg.models import PPLiteSeg
+from paddleseg.models.backbones import STDC1
+
+from .models import PPLiteSegRandomCrops
+from .train import train_model
+
+
+def train_model_command():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model_type',
+        help='Choose which type of model to train',
+        required=True,
+        type=str,
+        choices=['base', 'random-crops']
+    )
+    parser.add_argument(
+        '--dataset_root',
+        help='Set dataset root directory',
+        required=True,
+        type=str
+    )
+
+    args = parser.parse_args()
+    if args.model_type == 'base':
+        model = PPLiteSeg(
+            num_classes=2,
+            backbone=STDC1(pretrained='https://bj.bcebos.com/paddleseg/dygraph/PP_STDCNet1.tar.gz'),
+            arm_out_chs=[32, 64, 128],
+            seg_head_inter_chs=[32, 64, 128]
+        )
+        train_model(model, args.dataset_root)
+    else:
+        model = PPLiteSegRandomCrops(
+            num_classes=2,
+            backbone=STDC1(pretrained='https://bj.bcebos.com/paddleseg/dygraph/PP_STDCNet1.tar.gz'),
+            random_crops=3
+        )
+        train_model(model, args.dataset_root)
--- basketballtrainer/train.py
+++ basketballtrainer/train.py
@@ -1,5 +1,6 @@
-from paddleseg.models import PPLiteSeg
-from paddleseg.models.backbones import STDC1
+import pathlib
+
+import paddle.nn
 from paddleseg.models.losses import OhemCrossEntropyLoss
 from paddle.optimizer.lr import PolynomialDecay
 from paddle.optimizer.lr import LinearWarmup
@@ -9,36 +10,8 @@ import paddleseg.transforms as t
 from paddleseg.datasets import Dataset
 from paddleseg.core import train
 
-from basketballtrainer.models import PPLiteSegRandomCrops
 
-
-def train_model():
-    model = PPLiteSeg(
-        num_classes=2,
-        backbone=STDC1(pretrained='https://bj.bcebos.com/paddleseg/dygraph/PP_STDCNet1.tar.gz')
-    )
-
-    train_transforms = [
-        t.RandomHorizontalFlip(),
-        t.Normalize()
-    ]
-    val_transforms = [
-        t.Normalize()
-    ]
-
-    base_lr = 0.01
-    lr = PolynomialDecay(base_lr, power=0.9, decay_steps=1000, end_lr=0)
-    optimizer = Momentum(lr, parameters=model.parameters(), momentum=0.9, weight_decay=4.0e-5)
-    losses = {'types': [OhemCrossEntropyLoss(min_kept=200000)] * 3, 'coef': [1] * 3}
-
-
-def train_extended_model():
-    model = PPLiteSegRandomCrops(
-        num_classes=2,
-        backbone=STDC1(pretrained='https://bj.bcebos.com/paddleseg/dygraph/PP_STDCNet1.tar.gz'),
-        random_crops=3
-    )
-
+def train_model(model: paddle.nn.Layer, dataset_root: str):
     train_transforms = [
         t.ResizeStepScaling(min_scale_factor=0.5, max_scale_factor=2.0, scale_step_size=0.25),
         t.RandomPaddingCrop(crop_size=[1024, 512]),
@@ -50,26 +23,27 @@ def train_extended_model():
         t.Normalize()
     ]
 
+    dataset_path = pathlib.Path(dataset_root)
+    train_dataset = Dataset(
+        dataset_root=str(dataset_path),
+        transforms=train_transforms,
+        num_classes=2,
+        mode='train',
+        train_path=str(dataset_path / 'train.txt')
+    )
+    val_dataset = Dataset(
+        dataset_root=str(dataset_path),
+        transforms=val_transforms,
+        num_classes=2,
+        mode='val',
+        val_path=str(dataset_path / 'val.txt')
+    )
+
     losses = {'types': [OhemCrossEntropyLoss(min_kept=130000)] * 3, 'coef': [1] * 3}
     base_lr = 0.005
     lr = PolynomialDecay(base_lr, power=0.9, decay_steps=160000, end_lr=0)
     scheduler = LinearWarmup(lr, warmup_steps=1000, start_lr=1.0e-5, end_lr=base_lr)
     optimizer = Momentum(learning_rate=scheduler, parameters=model.parameters(), momentum=0.9, weight_decay=4.0e-5)
-
-    train_dataset = Dataset(
-        dataset_root='/mnt/DATA/tesi/dataset/dataset_paddleseg/',
-        transforms=train_transforms,
-        num_classes=2,
-        mode='train',
-        train_path='/mnt/DATA/tesi/dataset/dataset_paddleseg/train.txt'
-    )
-    val_dataset = Dataset(
-        dataset_root='/mnt/DATA/tesi/dataset/dataset_paddleseg/',
-        transforms=val_transforms,
-        num_classes=2,
-        mode='val',
-        val_path='/mnt/DATA/tesi/dataset/dataset_paddleseg/val.txt'
-    )
 
     train(
         model=model,
@@ -84,7 +58,3 @@ def train_extended_model():
         save_interval=5000,
         keep_checkpoint_max=32
     )
-
-
-if __name__ == '__main__':
-    train_extended_model()
