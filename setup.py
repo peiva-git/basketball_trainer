888f89fa1da355e986c60273708f632659cf5204
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-10-19 12:48:39 +0200

    Added website url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     long_description=readme,
     author='Ivan Pelizon',
     author_email='ivan.pelizon@gmail.com',
-    url='',
+    url='https://peiva-git.github.io/basketball_trainer/',
     license=license,
     packages=find_packages(exclude=('tests', 'docs', 'output', 'assets')),
     python_requires='>=3.8,<=3.11',
