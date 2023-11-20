e0bd61cdab35d5fa72a8bb601981ce3990038ac9
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-11-20 18:44:51 +0100

    Added data docs

--- basketballtrainer/data/__init__.py
+++ basketballtrainer/data/__init__.py
@@ -1,5 +1,7 @@
 """
-This package contains utilities to convert the provided dataset into various formats
+This package contains utilities to convert the provided dataset into various formats and to manipulate data in general.
+For more information on the format this project's dataset,
+take a look at the `basketballtrainer.data.convert_dataset` module.
 """
 
 from .convert_dataset import convert_dataset_to_paddleseg_format
