e2afcabdab28ff8bd6883137aa4ab65a6ed6eb85
Ivan Pelizon <24814264+peiva-git@users.noreply.github.com>
2023-11-20 18:58:30 +0100

    Added main docs

--- basketballtrainer/__init__.py
+++ basketballtrainer/__init__.py
@@ -1,2 +1,18 @@
+"""
+This is the root package of the [**BasketballTrainer project**](https://github.com/peiva-git/basketball_trainer).
+The main goal of this package is to train
+a model for the [**BasketballDetector tool**](https://github.com/peiva-git/basketball_detector).
+
+The model can be trained in two different ways:
+1. Using the PaddleSeg tools alongside the provided
+[configuration files](https://github.com/peiva-git/basketball_trainer/tree/master/configs).
+A detailed description with examples is available
+on [this project's public repository](https://github.com/peiva-git/basketball_trainer).
+2. Using the implemented terminal commands. More details are specified in the `basketballtrainer.cli` module.
+
+In both cases, the result of the training should be a `model.pdparams` file,
+required by the `basketballdetector` package.
+"""
+
 from .tasks import train_model
 from .tasks import evaluate_base_model, evaluate_rancrops_model
